fe09c13d8baa55dbf2dfbd19d99fe0e27da7cc60
Dany <danyramirezg@gmail.com>
2020-04-06 11:28:17 -0500

    first commit

---
+++ src/Contact_agent/Contact.java
@@ -0,0 +1,12 @@
+package Contact_agent;
+
+public class Contact {
+
+    private String name;
+    private String telephone;
+
+    public Contact(String name, String telephone) {
+        this.name = name;
+        this.name = telephone;
+    }
+}
