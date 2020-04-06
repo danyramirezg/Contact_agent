e50d526cb52a34a342dc90f60caa5c5a9de0d3c1
Dany <danyramirezg@gmail.com>
2020-04-06 13:13:48 -0500

    Add class Contact and Agenda to add new contacts

--- src/Contact_agent/Contact.java
+++ src/Contact_agent/Contact.java
@@ -9,4 +9,33 @@ public class Contact {
         this.name = name;
         this.name = telephone;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public boolean equals(Contact cont) {
+        if (this.name.equalsIgnoreCase(cont.getName())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + " Telephone: " + telephone;
+    }
 }
